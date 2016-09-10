79b91b676cb6423d6367c0073282fa8fcb318ba8
andraslesko93 <andras.lesko.93@gmail.com>
2016-09-10 19:27:57 +0200

    Welcome message added to fb login as well

--- SmartCampus/problems/pipeline.py
+++ SmartCampus/problems/pipeline.py
@@ -1,4 +1,6 @@
 from .models import UserProfile
+from problems.models import Notification
+from django.contrib.contenttypes.models import ContentType
 
 def create_user_profile(backend, user, response, details, *args, **kwargs):
     user_profile_match=UserProfile.objects.filter(user=user)
@@ -8,3 +10,7 @@ def create_user_profile(backend, user, response, details, *args, **kwargs):
         picture  = 'http://graph.facebook.com/{0}/picture?width=100'.format(response['id'])
         profile = UserProfile(user=user, picture_url=picture)
         profile.save()
+        welcome_notification = Notification(user = user,
+                                                content_type = ContentType.objects.get_for_model(user)
+                                                )
+        welcome_notification.save()
