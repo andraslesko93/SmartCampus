d13ee02d99f560da25cd3b97b4c45fba7f19c160
andraslesko93 <andras.lesko.93@gmail.com>
2016-11-02 20:15:51 +0100

    fixed static path problem

--- SmartCampus/SmartCampus/settings.py
+++ SmartCampus/SmartCampus/settings.py
@@ -84,21 +84,17 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
 
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 TEMPLATE_DIRS = (
    TEMPLATE_PATH,
 )
 
-STATIC_PATH = os.path.join(BASE_DIR,'static')
-
-STATIC_URL = '/static/' # You may find this is already defined as such.
-
-STATICFILES_DIRS = (
-    STATIC_PATH,
-)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
 
